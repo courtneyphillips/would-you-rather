9b3788404da4a9b30760fa08b28f161758a88c41
Courtney <courtney.mae.phillips@gmail.com>
2015-07-14 10:54:22 -0700

    Add routes and resources.

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,13 @@ Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
+  root :to => 'bouts#index'
+
+  resources :bouts do
+    resources :options
+    resources :comments
+  end
+
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
