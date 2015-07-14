bb323315e360e649ce81d50af136230c760177bf
Courtney <courtney.mae.phillips@gmail.com>
2015-07-14 12:13:51 -0700

    Add nested form and corresponding partials.

--- app/controllers/bouts_controller.rb
+++ app/controllers/bouts_controller.rb
@@ -10,16 +10,25 @@ class BoutsController < ApplicationController
 
   def new
     @bout = Bout.new
+    1.times {@bout.options.build}
   end
 
   def create
     @bout = Bout.new(bout_params)
+    # if @bout.save()
+    #   respond_to do |format|
+    #     format.html {redirect_to bouts_path}
+    #     format.js
+    #   end
+    # else
+    #   render :new
+    # end
   end
 
   private
 
   def bout_params
-    params.require(:bout).permit(:created_at)
+    params.require(:bout).permit(:created_at, options_attributes: [:description, :name])
   end
 
 
--- app/controllers/options_controller.rb
+++ app/controllers/options_controller.rb
@@ -3,25 +3,25 @@ class OptionsController < ApplicationController
   def index
     @options = Option.all
   end
+  # 
+  # def show
+  # end
+  #
+  # def new
+  #   @bout = Bout.find(params[:bout_id])
+  #   @option = @bout.options.new
+  # end
 
-  def show
-  end
-
-  def new
-    @bout = Bout.find(params[:bout_id])
-    @option = @bout.options.new
-  end
-
-  def create
-    @bout = Bout.find(params[:bout_id])
-    @option = @bout.options.create(option_params)
-    if @option.save
-      flash[:notice] = "Option saved!"
-      redirect_to bout_path(@bout)
-    else
-      render :new
-    end
-  end
+  # def create
+  #   @bout = Bout.find(params[:bout_id])
+  #   @option = @bout.options.create(option_params)
+  #   if @option.save
+  #     flash[:notice] = "Option saved!"
+  #     redirect_to bout_path(@bout)
+  #   else
+  #     render :new
+  #   end
+  # end
 
   private
 
